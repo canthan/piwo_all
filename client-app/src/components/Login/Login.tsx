5e1562f869272a22d062bd4ace93056d851af935
globisan <andrzej.globisz@ihg.com>
2019-07-29 14:06:14 +0200

    adding styles and validation for login form

--- client-app/src/components/Login/Login.tsx
+++ client-app/src/components/Login/Login.tsx
@@ -4,17 +4,24 @@ import { connect } from 'react-redux';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import { ErrorText } from '../Common/ErrorText/ErrorText';
-import { LoginSchema } from '../../constants/schemas';
-import { loginAsync } from '../../actions/app.actions';
-import { AsyncResult } from '../../types/common.types';
+import { ToastMessage } from '../Common/ToastMessage/ToastMessage';
 
+import { OverallAppState } from '../../reducers/initialState';
+import { loginAsync } from '../../actions/app.actions';
+import { LoginSchema } from '../../constants/schemas';
+import { Routes } from '../../constants/routes';
+
+import { AsyncResult } from '../../types/common.types';
 import { LOGIN_SUCCESS } from '../../constants/app.action.types';
 
 import './Login.scss';
-import { Routes } from '../../constants/routes';
 
 interface MappedActions {
   loginAsync(email: string, password: string, register: boolean): AsyncResult;
+}
+
+interface MappedProps {
+  error: string | null;
 }
 
 interface FormValues {
@@ -26,7 +33,7 @@ interface FormSubmitting {
   setSubmitting: (isSubmitting: boolean) => void,
 }
 
-type Props = MappedActions & RouterProps;
+type Props = MappedProps & MappedActions & RouterProps;
 
 export function Login(props: Props) {
   const loginAsync = (email: string, password: string, register: boolean): AsyncResult => props.loginAsync(email, password, register);
@@ -47,45 +54,57 @@ export function Login(props: Props) {
 
   return (
     <div className="container">
+      {
+        props.error ? <ToastMessage variant={'danger'}>{props.error}</ToastMessage> : null
+      }
       <div className="row justify-content-md-center">
         <div className="login col-4">
           <Formik
-            initialValues={{ email: 'andrzej.globisz@gmail.com', password: 'canthan12', register: false }}
+            initialValues={{ email: '', password: '', register: false }}
+            // initialValues={{ email: 'andrzej.globisz@gmail.com', password: 'canthan12', register: false }}
             validationSchema={LoginSchema}
             onSubmit={handleSubmit}
           >
-            {({ isSubmitting, setFieldValue, handleSubmit }) => (
-              <Form>
-                <div className="form-group login__form">
-                  <label className="login__form__label">Email</label>
-                  <Field type="email" className="form-control login__form__input" name="email" placeholder="Email address" />
-                  <ErrorMessage name="email" render={msg => ErrorText({ message: msg })} />
-                </div>
-                <div className="form-group login__form">
-                  <label className="login__form__label">Password</label>
-                  <Field type="password" className="form-control login__form__input" name="password" placeholder="Password" />
-                  <ErrorMessage name="password" render={msg => ErrorText({ message: msg })} />
-                </div>
-                <div className="form-group login__buttons">
-                  <button
-                    type="submit"
-                    disabled={isSubmitting}
-                    className="btn btn-primary col-7">
-                    Log in
+            {({ isSubmitting, setFieldValue, handleSubmit, errors }) => (
+                <Form>
+                  <div className="form-group common__form">
+                    <label className="common__form__label">Email</label>
+                    <Field
+                      type="email"
+                      className={`form-control common__form__input ${errors.email ? 'common__form__input--invalid' : ''} `}
+                      name="email"
+                      placeholder="Email address" />
+                    <ErrorMessage name="email" render={msg => ErrorText({ message: msg })} />
+                  </div>
+                  <div className="form-group common__form">
+                    <label className="common__form__label">Password</label>
+                    <Field
+                      type="password"
+                      className={`form-control common__form__input ${errors.password ? 'common__form__input--invalid' : ''}`}
+                      name="password"
+                      placeholder="Password" />
+                    <ErrorMessage name="password" render={msg => ErrorText({ message: msg })} />
+                  </div>
+                  <div className="form-group login__buttons">
+                    <button
+                      type="submit"
+                      disabled={isSubmitting || !!Object.entries(errors).length}
+                      className="btn btn-primary col-7">
+                      Log in
                     </button>
-                  <button
-                    type="submit"
-                    onClick={() => {
-                      setFieldValue('register', true);
-                      handleSubmit();
-                    }}
-                    disabled={isSubmitting}
-                    className="btn btn-outline-secondary col-4">
-                    Sign in
+                    <button
+                      type="submit"
+                      onClick={() => {
+                        setFieldValue('register', true);
+                        handleSubmit();
+                      }}
+                      disabled={isSubmitting || !!Object.entries(errors).length}
+                      className="btn btn-outline-secondary col-4">
+                      Sign in
                     </button>
-                </div>
-              </Form>
-            )}
+                  </div>
+                </Form>
+              )}
           </Formik>
         </div>
       </div>
@@ -93,7 +112,9 @@ export function Login(props: Props) {
   )
 }
 
-const mapStateToProps = () => ({})
+const mapStateToProps = (state: OverallAppState) => ({
+  error: state.app.error,
+})
 
 const actions = {
   loginAsync
